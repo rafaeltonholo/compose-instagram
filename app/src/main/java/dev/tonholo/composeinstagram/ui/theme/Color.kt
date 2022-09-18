b56d8dd4488cf90f888e53ee84542f67dd98389a
Rafael Tonholo <rafael@tonholo.dev>
2022-09-18 18:18:17 -0300

    feat: setup app colors

--- app/src/main/java/dev/tonholo/composeinstagram/ui/theme/Color.kt
+++ app/src/main/java/dev/tonholo/composeinstagram/ui/theme/Color.kt
@@ -1,8 +1,92 @@
 package dev.tonholo.composeinstagram.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+private val Lotion = Color(0xFFFAFAFA)
+private val CodGray = Color(0xFF1A1A1A)
+private val RaisinBlack = Color(0xFF262626)
+private val Azure = Color(0xFF0094F4)
+private val Coral = Color(0xFFFF3040)
+
+interface AppColors {
+    val primary: Color
+    val onPrimary: Color
+    val secondary: Color
+    val onSecondary: Color
+    val background: Color
+    val onBackground: Color
+    val surface: Color
+    val onSurface: Color
+    val surfaceVariant: Color
+    val onSurfaceVariant: Color
+    val error: Color
+    val onError: Color
+
+    fun toMaterial(darkTheme: Boolean): Colors = if (darkTheme) {
+        darkColors(
+            primary = primary,
+            primaryVariant = primary,
+            secondary = secondary,
+            secondaryVariant = secondary,
+            background = background,
+            surface = surface,
+            error = error,
+            onPrimary = onPrimary,
+            onSecondary = onSecondary,
+            onBackground = onBackground,
+            onSurface = onSurface,
+            onError = onError,
+        )
+    } else {
+        lightColors(
+            primary = primary,
+            primaryVariant = primary,
+            secondary = secondary,
+            secondaryVariant = secondary,
+            background = background,
+            surface = surface,
+            error = error,
+            onPrimary = onPrimary,
+            onSecondary = onSecondary,
+            onBackground = onBackground,
+            onSurface = onSurface,
+            onError = onError,
+        )
+    }
+}
+
+internal object LightColorsPalette : AppColors {
+    override val primary: Color = Azure
+    override val onPrimary: Color = Color.White
+    override val secondary: Color = Coral
+    override val onSecondary: Color = Color.White
+    override val background: Color = Color.White
+    override val onBackground: Color = RaisinBlack
+    override val surface: Color = Color.White
+    override val onSurface: Color = RaisinBlack
+    override val surfaceVariant: Color = Lotion
+    override val onSurfaceVariant: Color = CodGray
+    override val error: Color = Coral
+    override val onError: Color = Color.White
+}
+
+internal object DarkColorsPalette : AppColors {
+    override val primary: Color = Azure
+    override val onPrimary: Color = Color.White
+    override val secondary: Color = Coral
+    override val onSecondary: Color = Color.White
+    override val background: Color = Color.Black
+    override val onBackground: Color = Lotion
+    override val surface: Color = Color.Black
+    override val onSurface: Color = Lotion
+    override val surfaceVariant: Color = CodGray
+    override val onSurfaceVariant: Color = Lotion
+    override val error: Color = Coral
+    override val onError: Color = Color.White
+}
+
+internal val LocalAppColors = staticCompositionLocalOf<AppColors> { LightColorsPalette }
--- app/src/main/java/dev/tonholo/composeinstagram/ui/theme/Theme.kt
+++ app/src/main/java/dev/tonholo/composeinstagram/ui/theme/Theme.kt
@@ -1,47 +1,23 @@
 package dev.tonholo.composeinstagram.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 /**
  * Contains functions to access the current theme values provided at the call site's position in the hierarchy.
  */
 object Theme {
     /**
-     * Retrieves the current [Colors] at the call site's position in the hierarchy.
+     * Retrieves the current [AppColors] at the call site's position in the hierarchy.
      */
-    val colors: Colors
+    val colors: AppColors
         @Composable
         @ReadOnlyComposable
-        get() = MaterialTheme.colors
+        get() = LocalAppColors.current
 
     /**
      * Retrieves the current [AppTypography] at the call site's position in the hierarchy.
@@ -64,16 +40,17 @@ object Theme {
 @Composable
 fun ComposeInstagramTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        DarkColorsPalette
     } else {
-        LightColorPalette
+        LightColorsPalette
     }
 
     CompositionLocalProvider(
-        LocalTypography provides Typography
+        LocalTypography provides Typography,
+        LocalAppColors provides colors,
     ) {
         MaterialTheme(
-            colors = colors,
+            colors = colors.toMaterial(darkTheme),
             typography = Typography.toMaterial(),
             shapes = Shapes,
             content = content
