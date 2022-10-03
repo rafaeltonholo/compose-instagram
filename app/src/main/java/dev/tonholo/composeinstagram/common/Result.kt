32857d7579fc7c0e1129d5c405466482974b332f
Rafael Tonholo <rafael@tonholo.dev>
2022-10-03 18:25:22 -0300

    fix: added data support to Result.Error

--- app/src/main/java/dev/tonholo/composeinstagram/common/Result.kt
+++ app/src/main/java/dev/tonholo/composeinstagram/common/Result.kt
@@ -3,5 +3,9 @@ package dev.tonholo.composeinstagram.common
 sealed interface Result<T> {
     data class Success<T>(val data: T) : Result<T>
     data class Loading<T>(val data: T? = null) : Result<T>
-    data class Error<T>(val message: String, val throwable: Throwable? = null) : Result<T>
+    data class Error<T>(
+        val message: String,
+        val throwable: Throwable? = null,
+        val data: T? = null,
+    ) : Result<T>
 }
