4e2e9a2d00cd89b0bd3901b3778529d429eae71f
Rafael Tonholo <rafael@tonholo.dev>
2022-10-24 11:26:58 -0300

    chore: extracted home content to its own file

--- app/src/main/java/dev/tonholo/composeinstagram/feature/home/HomeScreen.kt
+++ app/src/main/java/dev/tonholo/composeinstagram/feature/home/HomeScreen.kt
@@ -1,11 +1,8 @@
 package dev.tonholo.composeinstagram.feature.home
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
@@ -14,22 +11,18 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tonholo.composeinstagram.data.fake.local.FakeUserDao
 import dev.tonholo.composeinstagram.data.fake.remote.FakePostApi
 import dev.tonholo.composeinstagram.data.fake.remote.FakeStoryApi
 import dev.tonholo.composeinstagram.feature.home.components.HomeAppBar
-import dev.tonholo.composeinstagram.feature.home.components.StoryRow
+import dev.tonholo.composeinstagram.feature.home.components.HomeContent
+import dev.tonholo.composeinstagram.feature.home.components.NavigationBottomBar
 import dev.tonholo.composeinstagram.feature.home.usercase.FetchHomePosts
 import dev.tonholo.composeinstagram.feature.home.usercase.FetchStories
 import dev.tonholo.composeinstagram.feature.home.usercase.FetchUserData
 import dev.tonholo.composeinstagram.feature.home.usercase.LikePost
-import dev.tonholo.composeinstagram.feature.post.ImagePost
-import dev.tonholo.composeinstagram.feature.home.components.NavigationBottomBar
 import dev.tonholo.composeinstagram.ui.theme.ComposeInstagramTheme
-import kotlinx.collections.immutable.toImmutableList
-import kotlinx.collections.immutable.toImmutableSet
 
 @Composable
 fun HomeScreen(
@@ -75,50 +68,19 @@ fun HomeScreen(
         },
         scaffoldState = scaffoldState,
     ) {
-        LazyColumn(
+        HomeContent(
+            storyItems = state.storyState.storyItems,
+            posts = state.postState.posts,
+            isEndReached = state.postState.isEndReached,
+            isLoading = state.postState.isLoading,
+            currentUserTag = state.userState.currentUser?.userTag,
+            currentUserProfileImage = state.userState.currentUser?.profileImage.orEmpty(),
+            onFetchNextPost = viewModel::fetchNextPosts,
+            onPostLiked = viewModel::onPostLiked,
+            onCommentClick = viewModel::onCommentClick,
             modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-        ) {
-            item {
-                state.storyState.storyItems?.let { stories ->
-                    StoryRow(
-                        storyItems = stories,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 8.dp, bottom = 8.dp),
-                    )
-                }
-            }
-
-            state.postState.posts?.let { posts ->
-                itemsIndexed(posts) { index, post ->
-                    LaunchedEffect(index >= posts.size - 1) {
-                        if (index >= posts.size - 1 && !state.postState.isEndReached && !state.postState.isLoading) {
-                            viewModel.fetchNextPosts()
-                        }
-                    }
-
-                    ImagePost(
-                        userTag = post.owner.userTag,
-                        profileImageUrl = post.owner.profileImage,
-                        hasStory = false,
-                        images = post.images.toImmutableList(),
-                        isPostLiked = post.likes.any { like -> like.userTag == state.userState.currentUser?.userTag },
-                        isPostSaved = false,
-                        likes = post.likes.toImmutableSet(),
-                        postDate = post.postDate,
-                        currentUserProfileImageUrl = state.userState.currentUser?.profileImage.orEmpty(),
-                        ownerComment = post.ownerComment,
-                        commentCount = post.comments?.size ?: 0,
-                        onPostLiked = { liked ->
-                            viewModel.onPostLiked(liked, post)
-                        },
-                        onCommentClick = viewModel::onCommentClick
-                    )
-                }
-            }
-        }
+                .padding(it),
+        )
     }
 }
 
---
+++ app/src/main/java/dev/tonholo/composeinstagram/feature/home/components/HomeContent.kt
@@ -0,0 +1,76 @@
+package dev.tonholo.composeinstagram.feature.home.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.tonholo.composeinstagram.domain.Post
+import dev.tonholo.composeinstagram.domain.UserTag
+import dev.tonholo.composeinstagram.feature.post.ImagePost
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+import kotlinx.collections.immutable.toImmutableSet
+
+@Composable
+fun HomeContent(
+    storyItems: ImmutableList<StoryItem>?,
+    posts: ImmutableList<Post>?,
+    isEndReached: Boolean,
+    isLoading: Boolean,
+    currentUserTag: UserTag?,
+    currentUserProfileImage: String,
+    onFetchNextPost: () -> Unit,
+    onPostLiked: (Boolean, Post) -> Unit,
+    onCommentClick: (String?) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        item {
+            storyItems?.let { stories ->
+                StoryRow(
+                    storyItems = stories,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp, bottom = 8.dp),
+                )
+            }
+        }
+
+        posts?.let { posts ->
+            itemsIndexed(posts) { index, post ->
+                LaunchedEffect(index >= posts.size - 1) {
+                    if (index >= posts.size - 1 && !isEndReached && !isLoading) {
+                        onFetchNextPost()
+                    }
+                }
+
+                ImagePost(
+                    userTag = post.owner.userTag,
+                    profileImageUrl = post.owner.profileImage,
+                    hasStory = false,
+                    images = post.images.toImmutableList(),
+                    isPostLiked = post.likes.any { like -> like.userTag == currentUserTag },
+                    isPostSaved = false,
+                    likes = post.likes.toImmutableSet(),
+                    postDate = post.postDate,
+                    currentUserProfileImageUrl = currentUserProfileImage,
+                    ownerComment = post.ownerComment,
+                    commentCount = post.comments?.size ?: 0,
+                    onPostLiked = { liked ->
+                        onPostLiked(liked, post)
+                    },
+                    onCommentClick = onCommentClick
+                )
+            }
+        }
+    }
+
+}
